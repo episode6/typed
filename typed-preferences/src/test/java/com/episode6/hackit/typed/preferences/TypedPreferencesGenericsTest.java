5dc56f5bf43625457ea07c681f08109f2aeda477
Geoff Hackett <ghackett@gmail.com>
2017-04-18 16:41:42 -0400

    test a more complex prefKey

--- typed-preferences/src/test/java/com/episode6/hackit/typed/preferences/TypedPreferencesGenericsTest.java
+++ typed-preferences/src/test/java/com/episode6/hackit/typed/preferences/TypedPreferencesGenericsTest.java
@@ -5,12 +5,14 @@ import com.episode6.hackit.mockspresso.Mockspresso;
 import com.episode6.hackit.mockspresso.annotation.RealObject;
 import com.episode6.hackit.typed.core.util.Supplier;
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.mockito.Mock;
 
 import java.lang.reflect.Type;
+import java.util.HashMap;
 
 import static org.fest.assertions.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
@@ -32,6 +34,21 @@ public class TypedPreferencesGenericsTest {
       });
   private static final NullablePrefKey<SimpleTestClass> SIMPLE_NULL_PREF = PREF_NAMESPACE.key(SimpleTestClass.class)
       .named("simpleNullTestPref")
+      .buildNullable();
+
+  private static final PrefKey<HashMap<String, SimpleTestClass>> COMPLEX_MAP_PREF = PREF_NAMESPACE.key(new TypeToken<HashMap<String, SimpleTestClass>>() {})
+      .named("complexMapPref")
+      .buildWithDefault(new Supplier<HashMap<String, SimpleTestClass>>() {
+        @Override
+        public HashMap<String, SimpleTestClass> get() {
+          HashMap<String, SimpleTestClass> map = new HashMap<>();
+          map.put("key1", new SimpleTestClass("value1"));
+          map.put("key2", new SimpleTestClass("value2"));
+          return map;
+        }
+      });
+  private static final NullablePrefKey<HashMap<String, SimpleTestClass>> COMPLEX_MAP_NULL_PREF = PREF_NAMESPACE.key(new TypeToken<HashMap<String, SimpleTestClass>>() {})
+      .named("complexMapNullPref")
       .buildNullable();
 
   @Rule public final Mockspresso.Rule mockspresso = Rules.mockspresso();
@@ -158,6 +175,119 @@ public class TypedPreferencesGenericsTest {
     verify(mEditor).commit();
   }
 
+  @Test
+  public void testComplexPrefDoesntExist() {
+    String keyName = COMPLEX_MAP_PREF.getKeyName().toString();
+
+    HashMap<String, SimpleTestClass> result = mTypedPreferences.get(COMPLEX_MAP_PREF);
+
+    verify(mSharedPreferences).contains(keyName);
+    assertThat(result)
+        .isNotNull()
+        .containsKey("key1")
+        .containsKey("key2");
+    assertThat(result.get("key1").value).isEqualTo("value1");
+    assertThat(result.get("key2").value).isEqualTo("value2");
+  }
+
+  @Test
+  public void testComplexPrefDoesExist() {
+    String keyName = COMPLEX_MAP_PREF.getKeyName().toString();
+    Type objectType = COMPLEX_MAP_PREF.getObjectType();
+    HashMap<String, SimpleTestClass> expected = new HashMap<>();
+    when(mSharedPreferences.contains(keyName)).thenReturn(true);
+    when(mSharedPreferences.getString(keyName, null)).thenReturn("newValueJson");
+    when(mGson.fromJson("newValueJson", objectType)).thenReturn(expected);
+
+    HashMap<String, SimpleTestClass> result = mTypedPreferences.get(COMPLEX_MAP_PREF);
+
+    verify(mSharedPreferences).contains(keyName);
+    verify(mSharedPreferences).getString(keyName, null);
+    verify(mGson).fromJson("newValueJson", objectType);
+    assertThat(result)
+        .isNotNull()
+        .isEqualTo(expected);
+  }
+
+  @Test
+  public void testSetComplexPref() {
+    String keyName = COMPLEX_MAP_PREF.getKeyName().toString();
+    HashMap<String, SimpleTestClass> newValue = new HashMap<>();
+    when(mGson.toJson(newValue)).thenReturn("newSetValueJson");
+
+    mTypedPreferences.edit()
+        .put(COMPLEX_MAP_PREF, newValue)
+        .commit();
+
+    verify(mSharedPreferences).edit();
+    verify(mGson).toJson(newValue);
+    verify(mEditor).putString(keyName, "newSetValueJson");
+    verify(mEditor).commit();
+  }
+
+  @Test(expected = NullPointerException.class)
+  public void testSetComplexPrefException() {
+    mTypedPreferences.edit()
+        .put(COMPLEX_MAP_PREF, null);
+  }
+
+  @Test
+  public void testNullComplexPrefDoesntExist() {
+    String keyName = COMPLEX_MAP_NULL_PREF.getKeyName().toString();
+
+    HashMap<String, SimpleTestClass> result = mTypedPreferences.get(COMPLEX_MAP_NULL_PREF);
+
+    verify(mSharedPreferences).contains(keyName);
+    assertThat(result).isNull();
+  }
+
+  @Test
+  public void testNullComplexPrefDoesExist() {
+    String keyName = COMPLEX_MAP_NULL_PREF.getKeyName().toString();
+    Type objectType = COMPLEX_MAP_NULL_PREF.getObjectType();
+    HashMap<String, SimpleTestClass> expected = new HashMap<>();
+    when(mSharedPreferences.contains(keyName)).thenReturn(true);
+    when(mSharedPreferences.getString(keyName, null)).thenReturn("newValueJson");
+    when(mGson.fromJson("newValueJson", objectType)).thenReturn(expected);
+
+    HashMap<String, SimpleTestClass> result = mTypedPreferences.get(COMPLEX_MAP_NULL_PREF);
+
+    verify(mSharedPreferences).contains(keyName);
+    verify(mSharedPreferences).getString(keyName, null);
+    verify(mGson).fromJson("newValueJson", objectType);
+    assertThat(result)
+        .isNotNull()
+        .isEqualTo(expected);
+  }
+
+  @Test
+  public void testSetComplexNullPref() {
+    String keyName = COMPLEX_MAP_NULL_PREF.getKeyName().toString();
+    HashMap<String, SimpleTestClass> newValue = new HashMap<>();
+    when(mGson.toJson(newValue)).thenReturn("newSetValueJson");
+
+    mTypedPreferences.edit()
+        .put(COMPLEX_MAP_NULL_PREF, newValue)
+        .apply();
+
+    verify(mSharedPreferences).edit();
+    verify(mGson).toJson(newValue);
+    verify(mEditor).putString(keyName, "newSetValueJson");
+    verify(mEditor).apply();
+  }
+
+  @Test
+  public void testSetComplexNullPrefNull() {
+    String keyName = COMPLEX_MAP_NULL_PREF.getKeyName().toString();
+
+    mTypedPreferences.edit()
+        .put(COMPLEX_MAP_NULL_PREF, null)
+        .commit();
+
+    verify(mSharedPreferences).edit();
+    verify(mEditor).remove(keyName);
+    verify(mEditor).commit();
+  }
 
   public static class SimpleTestClass {
     final String value;
